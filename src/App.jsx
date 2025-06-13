94f12e895d56dc9f83e2c5edfab4f6721e787054
PeterChen712 <rudypeter@gmail.com>
2025-06-13 12:11:02 +0800

    chore: Remove duplicate imports in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,6 @@ import { useState } from 'react'
 import AuthToggle from './components/AuthToggle'
 import './App.css'
 
-import { useState } from 'react'
-import AuthToggle from './components/AuthToggle'
-import './App.css'
-
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false)
   const [isDarkMode, setIsDarkMode] = useState(false)
